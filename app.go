392afd5b110921891496bca13bac94097c14c178
Stuart Sessions <ssessions@veracode.com>
2024-08-06 10:51:27 -0400

    starter testing

--- app.go
+++ app.go
@@ -1,2 +1,50 @@
 // Will possibly be the main driver file for the app
 // App
+package main
+
+import (
+	"html/template"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/gorilla/mux"
+)
+
+type Page struct {
+	Title string
+	Body  []byte
+}
+
+func viewHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/view/"):]
+	p, _ := loadPage(title)
+	t, _ := template.ParseFiles("view.html")
+	t.Execute(w, p)
+}
+
+func editHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/edit/"):]
+	p, err := loadPage(title)
+	if err != nil {
+		p = &Page{Title: title}
+	}
+	t, _ := template.ParseFiles("edit.html")
+	t.Execute(w, p)
+}
+
+func loadPage(title string) (*Page, error) {
+	filename := title + ".txt"
+	body, err := os.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+	return &Page{Title: title, Body: body}, nil
+}
+
+func main() {
+	router := mux.NewRouter()
+	router.HandleFunc("/view/", viewHandler)
+	router.HandleFunc("/edit/", editHandler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
--- src-app/routes/routes.go
+++ src-app/routes/routes.go
@@ -1,1 +1,9 @@
-//placeholder
+package routes
+
+/**
+The routes file will create a Mux router, and then define all the
+handlefunctions which will be run when a specified URL is received.
+- Then, the handler functions can pass funcitonality to controllers,
+so the controllers should have similar structure and be able to process a login, etc.
+
+*/
