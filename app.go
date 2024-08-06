c1d941ce68f8be5ea42550a0c9849b7fe4174f87
clydeshtino <clydeshtino@gmail.com>
2024-08-06 10:28:24 -0400

    Updated resources/structure

--- app.go
+++ app.go
@@ -1,2 +1,16 @@
 // Will possibly be the main driver file for the app
-// App
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func helloPage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Hello, world!")
+}
+
+func main() {
+	http.HandleFunc("/", helloPage)
+	http.ListenAndServe(" ", nil)
+}
