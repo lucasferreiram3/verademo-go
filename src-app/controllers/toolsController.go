8e001015a6006219457c48da67c752aa72aa2b92
kevliu43 <kevliu43@gmail.com>
2024-08-07 14:59:13 -0400

    removed unnecessary code before rendering in toolsController

--- src-app/controllers/toolsController.go
+++ src-app/controllers/toolsController.go
@@ -2,12 +2,9 @@ package controllers
 
 import (
 	"net/http"
-	"os"
 	"verademo-go/src-app/shared/view"
 )
 
 func ShowTools(w http.ResponseWriter, r *http.Request) {
-	filename := "tools.html"
-	body, _ := os.ReadFile(filename)
-	view.Render(w, "tools.html", body)
+	view.Render(w, "tools.html", []byte{})
 }
--- src-app/shared/view/view.go
+++ src-app/shared/view/view.go
@@ -8,9 +8,9 @@ import (
 // Set up templates
 var templates = template.Must(template.ParseGlob("templates/*.html"))
 
-// htmlData is a byte array read from our template files
-func Render(w http.ResponseWriter, filename string, htmlData []byte) {
-	err := templates.ExecuteTemplate(w, filename, htmlData)
+// data is a struct with the variables to be passed into the template
+func Render(w http.ResponseWriter, filename string, data []byte) {
+	err := templates.ExecuteTemplate(w, filename, data)
 
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
