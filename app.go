ec8f16f23c0b06a6428e0c537f48a53cedd93d52
kevliu43 <kevliu43@gmail.com>
2024-08-07 09:06:51 -0400

    refactored routes

--- app.go
+++ app.go
@@ -3,59 +3,17 @@ package main
 import (
 	"database/sql"
 	"fmt"
-	"html/template"
 	"log"
 	"net/http"
-	"os"
+	router "verademo/go/src-app/routes"
 	db "verademo/go/src-app/shared/db"
-
-	"github.com/gorilla/mux"
 )
-
-type Page struct {
-	Title string
-	Body  []byte
-}
-
-func viewHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Print("view")
-	title := r.URL.Path[len("/view/"):]
-	p, _ := loadPage(title)
-	t, _ := template.ParseFiles("view.html")
-	t.Execute(w, p)
-}
-
-func editHandler(w http.ResponseWriter, r *http.Request) {
-	title := r.URL.Path[len("/edit/"):]
-	p, err := loadPage(title)
-	if err != nil {
-		p = &Page{Title: title}
-	}
-	t, _ := template.ParseFiles("edit.html")
-	t.Execute(w, p)
-}
-
-func loadPage(title string) (*Page, error) {
-	filename := title + ".txt"
-	body, err := os.ReadFile(filename)
-	if err != nil {
-		return nil, err
-	}
-	return &Page{Title: title, Body: body}, nil
-}
-
-func feedHandler(w http.ResponseWriter, r *http.Request) {
-
-}
 
 func main() {
 	var database *sql.DB
 	database, _ = db.InitDB()
 	fmt.Print("s")
-	router := mux.NewRouter()
-	router.HandleFunc("/feed", feedHandler)
-	router.HandleFunc("/view/", viewHandler)
-	router.HandleFunc("/edit/", editHandler)
-	log.Fatal(http.ListenAndServe(":8080", router))
+
+	log.Fatal(http.ListenAndServe(":8080", router.Routes()))
 	database.Close()
 }
--- src-app/routes/routes.go
+++ src-app/routes/routes.go
@@ -1,5 +1,19 @@
 package routes
 
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+	"os"
+
+	"github.com/gorilla/mux"
+)
+
+type Page struct {
+	Title string
+	Body  []byte
+}
+
 /**
 The routes file will create a Mux router, and then define all the
 handlefunctions which will be run when a specified URL is received.
@@ -7,3 +21,43 @@ handlefunctions which will be run when a specified URL is received.
 so the controllers should have similar structure and be able to process a login, etc.
 
 */
+
+func viewHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Print("view")
+	title := r.URL.Path[len("/view/"):]
+	p, _ := loadPage(title)
+	t, _ := template.ParseFiles("view.html")
+	t.Execute(w, p)
+}
+
+func editHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/edit/"):]
+	p, err := loadPage(title)
+	if err != nil {
+		p = &Page{Title: title}
+	}
+	t, _ := template.ParseFiles("edit.html")
+	t.Execute(w, p)
+}
+
+func loadPage(title string) (*Page, error) {
+	filename := title + ".txt"
+	body, err := os.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+	return &Page{Title: title, Body: body}, nil
+}
+
+func feedHandler(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func Routes() *mux.Router {
+	router := mux.NewRouter()
+	router.HandleFunc("/feed", feedHandler)
+	router.HandleFunc("/view/", viewHandler)
+	router.HandleFunc("/edit/", editHandler)
+
+	return router
+}
