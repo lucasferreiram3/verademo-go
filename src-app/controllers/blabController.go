e07ce98c190dbb26f5f3bd9a87124d928bc08e52
kevliu43 <kevliu43@gmail.com>
2024-08-09 14:49:42 -0400

    show blab complete

--- src-app/controllers/blabController.go
+++ src-app/controllers/blabController.go
@@ -1,6 +1,7 @@
 package controllers
 
 import (
+	"database/sql"
 	"fmt"
 	"net/http"
 	"strconv"
@@ -24,7 +25,7 @@ var sqlBlabsForMe = `SELECT users.username, users.blab_name, blabs.content, blab
 func ShowFeed(w http.ResponseWriter, r *http.Request) {
 
 	// Struct for variables to pass to the feed template
-	type Outputs struct {
+	type FeedVars struct {
 		BlabsByOthers []models.Blab
 		BlabsByMe     []models.Blab
 		CurrentUser   string
@@ -44,7 +45,7 @@ func ShowFeed(w http.ResponseWriter, r *http.Request) {
 
 	log.Println("User is Logged In - continuing... UA=" + r.Header.Get("user-agent") + " U=" + username)
 
-	var outputs Outputs
+	var outputs FeedVars
 
 	// Set an error if one was given in response (usually taken from processFeed)
 	resError := w.Header().Get("errorMsg")
@@ -75,7 +76,7 @@ func ShowFeed(w http.ResponseWriter, r *http.Request) {
 		var author models.Blabber
 		var post models.Blab
 
-		if err := blabsForMeResults.Scan(&author.Username, &author.BlabName, &post.Content, &post.PostDate, &post.CommentCount, &post.Id); err != nil {
+		if err := blabsForMeResults.Scan(&author.Username, &author.BlabName, &post.Content, &post.PostDate, &post.CommentCount, &post.ID); err != nil {
 			errMsg := "Error reading data from 'Blabs for me' query:\n" + err.Error()
 			log.Println(errMsg)
 			outputs.Error = errMsg
@@ -113,7 +114,7 @@ func ShowFeed(w http.ResponseWriter, r *http.Request) {
 	for blabsByMeResults.Next() {
 		var post models.Blab
 
-		if err := blabsByMeResults.Scan(&post.Content, &post.PostDate, &post.CommentCount, &post.Id); err != nil {
+		if err := blabsByMeResults.Scan(&post.Content, &post.PostDate, &post.CommentCount, &post.ID); err != nil {
 			errMsg := "Error reading data from 'Blabs by me' query:\n" + err.Error()
 			log.Println(errMsg)
 			outputs.Error = errMsg
@@ -191,14 +192,14 @@ func MoreFeed(w http.ResponseWriter, r *http.Request) {
 		var author models.Blabber
 		var post models.Blab
 
-		if err := results.Scan(&author.Username, &author.BlabName, &post.Content, &post.PostDate, &post.CommentCount, &post.Id); err != nil {
+		if err := results.Scan(&author.Username, &author.BlabName, &post.Content, &post.PostDate, &post.CommentCount, &post.ID); err != nil {
 			errMsg := "Error reading data from 'more feed' query:\n" + err.Error()
 			log.Println(errMsg)
 			http.Redirect(w, r, "feed", http.StatusBadRequest)
 			return
 		}
 
-		ret += fmt.Sprintf(template, author.Username, post.Content, author.BlabName, models.Timestamp(post.PostDate), post.Id, post.CommentCount)
+		ret += fmt.Sprintf(template, author.Username, post.Content, author.BlabName, models.Timestamp(post.PostDate), post.ID, post.CommentCount)
 
 	}
 
@@ -256,4 +257,112 @@ func ProcessFeed(w http.ResponseWriter, r *http.Request) {
 	}
 
 	ShowFeed(w, r)
+}
+
+func ShowBlab(w http.ResponseWriter, r *http.Request) {
+
+	// Struct for variables to pass to the feed template
+	type BlabVars struct {
+		Content  string
+		BlabName string
+		BlabID   int
+		Comments []models.Comment
+		Error    string
+	}
+
+	blabidParam := r.URL.Query().Get("blabid")
+
+	// Check session username
+	sess := session.Instance(r)
+
+	if sess.Values["username"] == nil {
+		log.Println("User is not Logged In - redirecting...")
+		http.Redirect(w, r, "login?target=feed", http.StatusFound)
+		return
+	}
+
+	username := sess.Values["username"].(string)
+
+	log.Println("User is Logged In - continuing... UA=" + r.Header.Get("user-agent") + " U=" + username)
+
+	blabDetailsSql := "SELECT blabs.content, users.blab_name " +
+		"FROM blabs INNER JOIN users ON blabs.blabber = users.username " +
+		"WHERE blabs.blabid = ?;"
+
+	blabCommentsSql := "SELECT users.username, users.blab_name, comments.content, comments.timestamp " +
+		"FROM comments INNER JOIN users ON comments.blabber = users.username " +
+		"WHERE comments.blabid = ? ORDER BY comments.timestamp DESC;"
+
+	var outputs BlabVars
+
+	// Convert GET parameter to integer
+	blabid, err := strconv.Atoi(blabidParam)
+	if err != nil {
+		errMsg := "Error converting blab ID:" + blabidParam + " to integer:\n" + err.Error()
+		log.Println(errMsg)
+		w.Header().Add("errorMsg", errMsg)
+		http.Redirect(w, r, "feed", http.StatusBadRequest)
+		return
+	}
+
+	outputs.BlabID = blabid
+
+	// Get blabs from blabbers that are being listened to
+	log.Println("Executing query to get blab details")
+	blabDetailsResult := sqlite.DB.QueryRow(blabDetailsSql, blabid)
+	err = blabDetailsResult.Scan(&outputs.Content, &outputs.BlabName)
+	switch {
+	case err == sql.ErrNoRows:
+		errMsg := "No blab found with ID:" + blabidParam + "\n" + err.Error()
+		log.Println(errMsg)
+		outputs.Error = errMsg
+		view.Render(w, "blab.html", outputs)
+		return
+	case err != nil:
+		errMsg := "Error getting blab details:\n" + err.Error()
+		log.Println(errMsg)
+		outputs.Error = errMsg
+		view.Render(w, "blab.html", outputs)
+		return
+	}
+
+	// Get the blab's comments
+	log.Println("Executing query to get all comments for blab")
+	blabCommentsResults, err := sqlite.DB.Query(blabCommentsSql, blabid)
+	if err != nil {
+		errMsg := "Error getting blab comments:\n" + err.Error()
+		log.Println(errMsg)
+		outputs.Error = errMsg
+		view.Render(w, "blab.html", outputs)
+		return
+	}
+
+	// Close the results object when they have been used up
+	defer blabCommentsResults.Close()
+
+	// Add each comment found to a variable to be passed to the template
+	var comments []models.Comment
+
+	for blabCommentsResults.Next() {
+		var author models.Blabber
+		var comment models.Comment
+
+		if err := blabCommentsResults.Scan(&author.Username, &author.BlabName, &comment.Content, &comment.PostDate); err != nil {
+			errMsg := "Error reading data from blab comments:\n" + err.Error()
+			log.Println(errMsg)
+			outputs.Error = errMsg
+			view.Render(w, "blab.html", outputs)
+			return
+		}
+
+		comment.Author = author
+		comment.PostDate = models.Timestamp(comment.PostDate)
+
+		comments = append(comments, comment)
+
+	}
+
+	outputs.Comments = comments
+
+	view.Render(w, "blab.html", outputs)
 }
