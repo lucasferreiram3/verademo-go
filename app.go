5ed49d4465827dfdc8cbbd901c7502c729523b54
kevliu43 <kevliu43@gmail.com>
2024-08-13 12:46:17 -0400

    embedded template/resources files and fixed file paths to work when the program is run from anywhere

--- app.go
+++ app.go
@@ -1,22 +1,28 @@
 package main
 
 import (
-	"database/sql"
+	"embed"
 	"log"
 	"net/http"
 	router "verademo-go/src-app/routes"
-	db "verademo-go/src-app/shared/db"
+	sqlite "verademo-go/src-app/shared/db"
 	session "verademo-go/src-app/shared/session"
+	"verademo-go/src-app/shared/view"
 )
+
+//go:embed resources
+var resources embed.FS
+
+//go:embed templates
+var templates embed.FS
 
 func main() {
 
 	session.Configure(session.Session{Name: "verademo", SecretKey: "key"})
-	var database *sql.DB
-	database, _ = db.OpenDB()
+	sqlite.OpenDB()
 	log.Print("\nStarting VerademoGO....")
 	log.Print("\nVerademoGO is running.")
-
+	view.ParseTemplates(templates)
+	router.SetResources(resources)
 	log.Fatal(http.ListenAndServe(":8000", router.Routes()))
-	database.Close()
 }
--- src-app/shared/db/db.go
+++ src-app/shared/db/db.go
@@ -2,6 +2,9 @@ package db
 
 import (
 	"database/sql"
+	"log"
+	"path/filepath"
+	"runtime"
 
 	_ "github.com/mattn/go-sqlite3"
 )
@@ -11,8 +14,22 @@ var DB *sql.DB
 func OpenDB() (*sql.DB, error) {
 	var err error
 
-	DB, err = sql.Open("sqlite3", "db.sqlite3")
+	// Get the current file
+	_, currentFile, _, ok := runtime.Caller(0)
+	if !ok {
+		errMsg := "Error getting current file."
+		log.Println(errMsg)
+		return nil, err
+	}
+
+	// Get the path to the database folder
+	dir := filepath.Join(filepath.Dir(currentFile), "..", "..", "..", "db")
+
+	// Open the database
+	DB, err = sql.Open("sqlite3", filepath.Join(dir, "db.sqlite3"))
 	if err != nil {
+		errMsg := "Error opening database: \n" + err.Error()
+		log.Println(errMsg)
 		return nil, err
 	}
 
